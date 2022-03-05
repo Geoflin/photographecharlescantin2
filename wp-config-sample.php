7660fae13d8281ed6da87142f9a20a13db064235
Guillaume Baur <guilaume.baur@gmail.com>
2022-03-05 17:08:31 +0100

    restore3

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -22,16 +22,16 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'uao6bj7832ejjozx' );
+define( 'DB_NAME', 'votre_nom_de_bdd' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'gqpiug8jz5zy6piu' );
+define( 'DB_USER', 'votre_utilisateur_de_bdd' );
 
 /** Mot de passe de la base de données MySQL. */
-define( 'DB_PASSWORD', 'rioyg81i5ak4gq89' );
+define( 'DB_PASSWORD', 'votre_mdp_de_bdd' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com	' );
+define( 'DB_HOST', 'localhost' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
